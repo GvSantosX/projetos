9df9b2d6ef9a07b296ff18f48b9fa8c4e2f8e7ad
GvSantosX <giovani.santos.barbosa@gmail.com>
2020-12-18 22:22:20 -0300

    form animado pt2

--- anim_form/script.js
+++ anim_form/script.js
@@ -1,6 +1,7 @@
 var btnLogin = document.querySelector(".btn-login");
 var form = document.querySelector("form");
 
-btnLogin.addEventListener("click", (event) = {
-    event
-})
+btnLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+    form.classList.add("form-hide")
+});
